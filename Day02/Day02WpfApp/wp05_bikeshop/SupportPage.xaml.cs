1975948a69513e086c21a7b785d7be666a250f85
yeseoz <w12345678992@gmail.com>
2023-04-19 17:28:06 +0900

    WPF 2일차 계속해서 마음에 안듬

---
+++ Day02/Day02WpfApp/wp05_bikeshop/SupportPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using wp05_bikeshop.Logics;
+
+namespace wp05_bikeshop
+{
+    /// <summary>
+    /// SupportPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SupportPage : Page
+    {
+        Car myCar = null;
+
+        public SupportPage()
+        {
+            InitializeComponent();
+            InitCar();
+        }
+
+        private void InitCar()
+        {
+            // 일반적인 C#에서 클래스 객체 인스턴스 사용방법 동일
+            myCar = new Car();
+            myCar.Names = "아이오닉";
+            myCar.Colors = Colors.White;
+            myCar.Speed = 220;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TxtSample.Text = myCar.Speed.ToString(); // 전통적인 윈폼 방식
+            // wpf로 할수 있는 최신기술 사용 못함
+        }
+    }
+}
